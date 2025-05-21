1f432734333a26801a32d5cb1d01f198425726fc
Garcia019 <matheugarcia2010@gmail.com>
2025-05-20 23:43:42 -0300

    feat: adiciona aulas finais da seção 06 e desafio concluído

--- 02_Exercícios/s06_desafio_circulo.py
+++ 02_Exercícios/s06_desafio_circulo.py
@@ -1,18 +1,35 @@
-# VERSÃO ÁREA DA CIRCUNFERÊNCIA V10.0
+# VERSÃO ÁREA DA CIRCUNFERÊNCIA V14.0
 # Utilizar o prefixo python antes de executar pelo navegador
 from math import pi
 import sys
+import errno
+
+
+class TerminalColor:
+    ERRO = "\033[091m"
+    NORMAL = "\033[0m"
 
 
 def circulo(raio):
     return pi * float(raio) ** 2
 
 
+def help():
+    print("É necessário informar o raio do círculo.")
+    print(f"Sintaxe: {sys.argv[0][2:]} <raio>")
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("É necessário informar o raio do círculo.")
-        print(f"Sintaxe: {sys.argv[0][2:]} <raio>")
-    else:
-        raio = sys.argv[1]
-        area = circulo(raio)
-        print(f"Á área da circunferência é de {area:.2f}")
+        help()
+        sys.exit(errno.EPERM)
+    elif not sys.argv[1].isnumeric():
+        help()
+        print(TerminalColor.ERRO +
+              "O raio informado deve ser no formato numérico" +
+              TerminalColor.NORMAL)
+        # NESSE PRINT É NECESSÁRIO ALTERAR A COR E RETORNAR
+        sys.exit(errno.EINVAL)
+    raio = sys.argv[1]
+    area = circulo(raio)
+    print(f"Á área da circunferência é de {area:.2f}")
--- 02_Exercícios/sistema_cadastro_estatistica.py
+++ 02_Exercícios/sistema_cadastro_estatistica.py
@@ -1,5 +1,49 @@
+"""
+Desafio: Sistema de Gestão de Pessoas (SGP)
+
+Objetivo:
+Construir um programa interativo via terminal que simule o sistema de cadastro
+de uma academia.
+O sistema deve registrar informações básicas dos usuários e fornecer
+estatísticas sobre os dados coletados.
+
+Regras do sistema:
+
+1. O sistema deve exibir um menu com opções numeradas de 1 a 10.
+2. A opção 1 deve permitir o cadastro de uma nova pessoa, pedindo:
+   - Nome
+   - Idade
+   - Peso (kg)
+   - Altura (m)
+   - Gênero (M/F)
+   - Está matriculado no plano de musculação? (S/N)
+3. Os dados de cada pessoa devem ser armazenados em um dicionário.
+   Todos os cadastros devem ser salvos em uma lista chamada 'cadastros'.
+
+4. As demais opções do menu devem:
+   2 → Mostrar o número total de pessoas cadastradas.
+   3 → Mostrar a quantidade de pessoas do gênero masculino e feminino.
+   4 → Mostrar quantas pessoas estão matriculadas no plano de musculação.
+   5 → Calcular e exibir a média de idade dos cadastrados.
+   6 → Calcular e exibir a média de IMC (peso / altura²) dos cadastrados.
+   7 → Exibir o nome e IMC da pessoa com o maior IMC.
+   8 → Exibir o nome e IMC da pessoa com o menor IMC.
+   9 → Permitir buscar um cadastro pelo nome e exibir os dados encontrados.
+  10 → Encerrar o programa.
+
+Regras adicionais:
+- Deve haver validação para evitar divisões por zero (ex: lista vazia).
+- A exibição de valores como IMC deve ser formatada com 2 casas decimais.
+- O sistema deve aceitar nomes repetidos, mas mostrar todos os registros
+correspondentes ao buscar.
+- O sistema deve continuar rodando até que o usuário selecione a opção 10.
+
+Conteúdo coberto: input, listas, dicionários, funções básicas, condicionais,
+laços, lógica e exibição.
+"""
+
 cadastros = []
-while(True):
+while (True):
     print("SGP - Sistema de Gestão de Pessoas")
     print(
         """
@@ -21,9 +65,12 @@ while(True):
         pessoa["nome"] = input("Digite o nome da Pessoa:").lower()
         pessoa["idade"] = int(input(f"Digite a idade da {pessoa['nome']}:"))
         pessoa["peso"] = float(input(f"Digite o peso da {pessoa['nome']}:"))
-        pessoa["altura"] = float(input(f"Digite a altura da {pessoa['nome']}:"))
-        pessoa["genero"] = input(f"Digite o gênero da {pessoa['nome']} M ou F: ").upper()
-        if input(f"A {pessoa['nome']} está matriculada no plano de musculação S OU N:") == "S":
+        pessoa["altura"] = float(input(
+            f"Digite a altura da {pessoa['nome']}:"))
+        pessoa["genero"] = input(
+            f"Digite o gênero da {pessoa['nome']} M ou F: ").upper()
+        if input(f"""A {pessoa['nome']}
+                 está matriculada no plano de musculação S OU N:""") == "S":
             pessoa["plano"] = True
         else:
             pessoa["plano"] = False
@@ -39,13 +86,15 @@ while(True):
                 contagem_homens += 1
             else:
                 contagem_mulheres += 1
-        print(f"Cadastros nesse software possuem {contagem_homens} Homem(ns) e {contagem_mulheres} Mulher(ss).")
+        print(f"""Cadastros nesse software possuem {contagem_homens}
+              Homem(ns) e {contagem_mulheres} Mulher(ss).""")
     elif menu == 4:
         numero_pessoas_matriculadas = 0
         for individuo in cadastros:
             if individuo["plano"]:
                 numero_pessoas_matriculadas += 1
-        print(f"Atualmente há {numero_pessoas_matriculadas} pessoas matriculadas no plano de musculação.")
+        print(f"""Atualmente há {numero_pessoas_matriculadas}
+              pessoas matriculadas no plano de musculação.""")
     elif menu == 5:
         if len(cadastros) == 0:
             print("Lista de Cadastro Vazia")
@@ -53,7 +102,8 @@ while(True):
             somatorio_idades = 0
             for individuo in cadastros:
                 somatorio_idades += individuo["idade"]
-            print(f"A média de idade das pessoas cadastradas é de {somatorio_idades/len(cadastros)}.")
+            print(f"""A média de idade das pessoas
+                  cadastradas é de {somatorio_idades/len(cadastros)}.""")
     elif menu == 6:
         if len(cadastros) == 0:
             print("Lista de Cadastro Vazia")
@@ -61,27 +111,35 @@ while(True):
             imc_total = 0
             for individuo in cadastros:
                 imc_total += individuo["peso"] / individuo["altura"] ** 2
-            print(f"A média de IMC das pessoas cadastradas é de {imc_total/len(cadastros)}.")
+            print(f"""A média de IMC das pessoas
+                  cadastradas é de {imc_total/len(cadastros)}.""")
     elif menu == 7:
         if len(cadastros) == 0:
             print("Lista de Cadastro Vazia")
         else:
             maior_imc = 0
+            pessoa_maior_imc = ""
             for individuo in cadastros:
                 if (individuo["peso"] / individuo["altura"] ** 2) > maior_imc:
-                    maior_imc = individuo["nome"]
-            print(f"A maior IMC do cadastro pertence ao {maior_imc}!")
+                    maior_imc = individuo["peso"] / individuo["altura"] ** 2
+                    pessoa_maior_imc = individuo["nome"]
+            print(f"""A maior IMC do cadastro pertence ao
+                  {pessoa_maior_imc} com o {maior_imc} de IMC!""")
     elif menu == 8:
         if len(cadastros) == 0:
             print("Lista de Cadastro Vazia")
         else:
             menor_imc = 200
+            pessoa_menor_imc = ""
             for individuo in cadastros:
                 if (individuo["peso"] / individuo["altura"] ** 2) < menor_imc:
-                    menor_imc = individuo["nome"]
-            print(f"O menor IMC do cadastro pertence ao {menor_imc}!")
+                    menor_imc = individuo["peso"] / individuo["altura"] ** 2
+                    pessoa_menor_imc = individuo["nome"]
+            print(f"""O menor IMC do cadastro pertence ao
+                  {pessoa_menor_imc} com o {menor_imc} de IMC!""")
     elif menu == 9:
-        nome = input("Digite o nome do usuário que deseja buscar no banco de dados do cadastro:").lower()
+        nome = input("""Digite o nome do usuário que deseja
+                     buscar no banco de dados do cadastro:""").lower()
         print("Segue informações do cadastrado:")
         for individuo in cadastros:
             if individuo["nome"] == nome:
