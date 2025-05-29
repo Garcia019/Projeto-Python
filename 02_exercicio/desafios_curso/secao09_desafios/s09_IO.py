b97a6e2c9cea8c8a08c3ee89f7921a215323d577
Garcia019 <matheugarcia2010@gmail.com>
2025-05-28 23:15:10 -0300

    feat: finalizando a implementação final de todas as versões desse algoritmo de criação de arquivo

--- 02_exercicio/desafios_curso/secao09_desafios/s09_IO.py
+++ 02_exercicio/desafios_curso/secao09_desafios/s09_IO.py
@@ -1,7 +1,26 @@
-# IO - version_3.0
-arquivo = open("pessoas.csv")
+# IO - version_5.0
 
-for registro in arquivo:
-    print("Nome: {}, Idade: {}".format(*registro.strip().split(",")))
+"""
+Já podemos considerar que a função open()
+tem dois parâmetros um deles é o nome do arquivo
+que será aberto e o segundo parâmetro sera o
+mode de execução do arquivo que você pode determinar
+no exemplo abaixo vamos abrir o arquivo "pessoas.txt"
+no modo de escrita utilizando o parâmetro "w"
 
-arquivo.close()
+Outro detalhe é sobre a utilização do parâmetro file
+na função saída, que ao invés de fazer a impressão
+no console faz diretamente na memoria saida, nesse
+caso o arquivo pessoa.txt
+"""
+with open("pessoas.csv",) as arquivo:
+    with open("pessoas.txt", "w") as saida:
+        for registro in arquivo:
+            pessoa = registro.strip().split(",")
+            print("Nome: {}, Idade: {}".format(*pessoa), file=saida)
+
+if arquivo.closed:
+    print("Arquivo já foi fechado por conta do bloco de execução with!")
+
+if saida.closed:
+    print("Arquivo de saída já foi fechado conta do bloco de execução with!")
