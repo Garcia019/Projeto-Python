47797888a7534c508e710d80376a4a89ceb54425
Garcia019 <matheugarcia2010@gmail.com>
2025-06-03 23:54:44 -0300

    feat: adicionando novas aulas seção 11

--- 02_exercicio/desafios_curso/secao11_desafios/gerador_html.py
+++ 02_exercicio/desafios_curso/secao11_desafios/gerador_html.py
@@ -1,38 +1,36 @@
-# Gerador HTML - version-v4.0
+# Gerador HTML - version-v5.0
+bloco_atributos = ("bloco_accesskey", "bloco_id")
+ul_atributos = ("ul_id", "ul_style")
 
-# Ao utilizar parâmetros especiais de empacotamento ou desempacotamento
-# Devemos nomear todos os parâmetros da função para o uso correto da mesma
-# Nesse caso não podemos passar nenhum parâmetros posicionais
-# todos os parâmetros devem ser parâmetros nomeados
-# se passarmos 2 ou mais parâmetros pois
-# a posição do *args é no segundo parâmetro
-def tag_bloco(conteudo, *args, classe="success", inline=False):
-    # Adicionando um novo parâmetro opcional a função
+
+def filtrar_atributos(informados, suportados):
+    return "".join(f" {chave.split('_')[-1]}='{valor}'"
+                   for chave, valor in informados.items()
+                   if chave in suportados)
+
+
+def tag_bloco(conteudo, *args, classe="success",
+              inline=False, **novos_atributos):
     tag = "span" if inline else "div"
-    html = conteudo if not callable(conteudo) else conteudo(*args)
-    return f"<{tag} class='{classe}'>{html}</{tag}>"
+    html = conteudo if not callable(conteudo)\
+        else conteudo(*args, **novos_atributos)
+    atributos = filtrar_atributos(novos_atributos, bloco_atributos)
+    return f"<{tag}{atributos} class='{classe}'>{html}</{tag}>"
 
 
-# No caso essa função utiliza do empacotamento de todas as variáveis passadas
-# cria uma tupla com essa variáveis
-def tag_lista(*itens):
+def tag_lista(*itens, **novos_atributos):
     lista = "".join(f"<li>{item}</li>" for item in itens)
-    return f"<ul>{lista}</ul>"
+    return f"<ul{filtrar_atributos(novos_atributos,ul_atributos)}>{lista}</ul>"
 
 
 if __name__ == "__main__":
     print(tag_bloco("bloco"))
     print(tag_bloco("inline e classe", classe="info", inline=True))
-    # Utilizando Parâmetros de forma Posicional
     print(tag_bloco("inline", inline=True))
-    # Nesse caso utilizando o parâmetro Nomeado
     print(tag_bloco(inline=True, conteudo="inline"))
-    # Nesse caso utilizando o parâmetro Nomeado
     print(tag_bloco("falhou", classe="erro"))
     print(tag_bloco(tag_lista("item1", "item2"), classe="info"))
-    # Ao utilizar parâmetros especiais de empacotamento ou desempacotamento
-    # Devemos nomear todos os parâmetros da função para o uso correto da mesma
-    # Nesse caso não podemos passar nenhum parâmetros posicionais
-    # todos os parâmetros devem ser parâmetros nomeados
     print(tag_bloco(tag_lista, "Sábado", "Domingo",
                     classe="info", inline=True))
+    print(tag_bloco(tag_lista, "Item1", "Item2", classe="info",
+                    bloco_accesskey="m", bloco_id="conteudo", ul_id="lista"))
